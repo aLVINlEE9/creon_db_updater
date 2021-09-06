bde7408e2c5f1edad80415c77fe5d0671daf906a
aLVINlEE9 <fldh3746@gmail.com>
2021-09-07 00:21:59 +0900

    add Analyzer moudle

---
+++ Analyzer_Module/Analyzer_for_minute.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import pymysql
+from datetime import datetime
+from datetime import timedelta
+import re
+
+class MinuteDB:
+    def __init__(self):
+        """생성자: MariaDB 연결 및 종목코드 딕셔너리 생성"""
+        self.conn = pymysql.connect(host='?', user='?', 
+            password='****', db='?', charset='utf8')
+        self.codes = {}
+        self.get_comp_info()
+        
+    def __del__(self):
+        """소멸자: MariaDB 연결 해제"""
+        self.conn.close()
+
+    def get_comp_info(self):
+        """company_info 테이블에서 읽어와서 codes에 저장"""
+        sql = "SELECT * FROM company_info"
+        krx = pd.read_sql(sql, self.conn)
+        for idx in range(len(krx)):
+            self.codes[krx['code'].values[idx]] = krx['company'].values[idx]
+
+    def get_date_db(self, code):
+        sql = f"SELECT DISTINCT date FROM minute_db WHERE code = '{code}'"
+        df = pd.read_sql(sql, self.conn)
+        return df 
+
+    def get_minute_db(self, code, start_date=None):
+        
+        if start_date is None:
+            one_year_ago = datetime.today() - timedelta(days=365)
+            start_date = one_year_ago.strftime('%Y-%m-%d')
+            print("start_date is initialized to '{}'".format(start_date))
+        else:
+            start_lst = re.split('\D+', start_date)
+            if start_lst[0] == '':
+                start_lst = start_lst[1:]
+            start_year = int(start_lst[0])
+            start_month = int(start_lst[1])
+            start_day = int(start_lst[2])
+            if start_year < 1900 or start_year > 2200:
+                print(f"ValueError: start_year({start_year:d}) is wrong.")
+                return
+            if start_month < 1 or start_month > 12:
+                print(f"ValueError: start_month({start_month:d}) is wrong.")
+                return
+            if start_day < 1 or start_day > 31:
+                print(f"ValueError: start_day({start_day:d}) is wrong.")
+                return
+            start_date=f"{start_year:04d}-{start_month:02d}-{start_day:02d}"
+
+        sql = f"SELECT * FROM minute_db WHERE code = '{code}' and date = '{start_date}'"
+        df = pd.read_sql(sql, self.conn)
+        return df 
